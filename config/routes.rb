027891b185ebefff4b2a4a14302d7d338ce9fbc7
Prelang Builder <builder@prelang.com>
2014-06-21 00:58:54 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Yeti::Application.routes.draw do
+  get "landings/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
